5df0de07a63b3833371647d7145e6c8170ccfaee
darshandama127277-lab <darshan.dama127277@marwadiuniversity.ac.in>
2026-06-12 12:18:40 +0530

    Add files via upload

---
+++ result.php
@@ -0,0 +1,16 @@
+Write a PHP program for print Previous semester Result using variables & constants in PHP.
+
+<?php
+    echo "+---------------------------------------------+<br>";
+    echo "|               MARWADI UNIVERSITY            |<br>";
+    echo "+---------------------------------------------+<br>";
+    echo "|NAME : DARSHAN DAMA                          |<br>";
+    echo "|SEM  : 4                                     |<br>";
+    echo "|GRNO : 127277                                |<br>";
+    echo "|ROLL NO. : 92400527175                       |<br>";
+    echo "+---------------------------------------------+<br>";
+    echo "|C# : A++                                     |<br>";
+    echo "|CN : A+                                      |<br>";
+    echo "|OS : B+                                      |<br>";
+    echo "+---------------------------------------------+<br>";
+?>
