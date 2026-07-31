50efe72c6e2c3faa457e6acec9a372b76c1c9752
darshandama127277-lab <darshan.dama127277@marwadiuniversity.ac.in>
2026-07-31 10:53:50 +0530

    Add files via upload

---
+++ demoajx.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        function loadText(){
+            var xhr = XMLHttpRequest();
+
+            xhr.open("GET","sample.txt", true;)
+
+            xhr.onreadystatechange = function ()
+            {
+                if (xhr.readyState == 4 &&)
+            }
+        }
+</body>
+</html>
